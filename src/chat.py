e16e69c1eb18e5262cda9ff1df3ec79d57d6a3bc
Petar Dosev <pdosev@gmail.com>
2021-08-04 14:40:10 -0400

    Working chat with websockets!

--- src/chat.py
+++ src/chat.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, send_from_directory
+from . import socketio
+from flask_socketio import emit
 
 bp = Blueprint("chat", __name__)
 
 @bp.route("/chat/<path:path>")
 def chat(path):
 	return send_from_directory('static', path)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('we got a message!')
+
+@socketio.on('event')
+def handle_event(json, methods=['GET', 'POST']):
+    print('received an event: '+ str(json))
+    socketio.emit('response', json, callback=messageReceived)
